3f47379594b8a543cab6066b0d36baa5eeebcb5e
XerXez7 <51115343+IyXerXez72@users.noreply.github.com>
2020-02-06 22:31:08 +0700

    Add files via upload

---
+++ sambung.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+os.system("php index1.php")
+def usage():
+        os.system("")
+tools = input(">>>Pilih<<< ")
+
+if tools ==1:
+        os.system("php lag.php")
+	os.system("python2 in.py")
+	os.syatem("sh index.sh")
+        print ("Done")
+
+if tools ==2:
+        os.system("php laga.php")
+        os.system("python2 on.py")
+        os.syatem("sh index.sh")
+        print ("Done")
+
+if tools ==3:
+        os.system("cd remove")
+        os.system("sh index.sh")
+        print ("Done")
