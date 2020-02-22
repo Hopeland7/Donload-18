076e76f58a5ab6c834aa71898e17ad7309adbf16
XerXez7 <51115343+IyXerXez72@users.noreply.github.com>
2020-02-21 16:45:31 -0800

    Create script.js

---
+++ new/script.js
@@ -0,0 +1,96 @@
+
+window.onload = function () {
+
+    var a = document.getElementById('a');
+    var b = document.getElementById('b');
+    var pic = document.getElementById('pic');
+
+    setTimeout(function () {
+        a.style.animation = 'slide_right 25s';
+        a.style.animationFillMode='forwards';
+
+        b.style.animation = 'slide_left 15s';
+        b.style.animationFillMode='forwards';
+
+        setTimeout(function () {
+            a.innerHTML="IKA. Seorang cewe sad yang bayak gaya";
+            a.style.opacity=1;
+            a.style.animation = 'slide_left 15s';
+            setTimeout(function () {
+                a.innerHTML="ANUGRAH Ctr. Seorang anak nolep tapi pacarya cans tapi kaga sih";
+                a.style.opacity=1;
+                a.style.animation = 'slide_right 15s';
+                a.style.animationFillMode='forwards';
+
+                setTimeout(function () {
+                    a.innerHTML="Z3X. Seorang pendiri Team bojonegoro cyber yang sangat sad boys";
+                    a.style.opacity=1;
+                    a.style.animation = 'slide_left 15s';
+                    a.style.animationFillMode='forwards';
+
+                    b.innerHTML="Sin X. Adalah mantanya zex yang tapernah terlupakan";
+                    b.style.opacity=1;
+                    b.style.animation = 'slide_right 15s';
+                    b.style.animationFillMode='forwards';
+
+                    setTimeout(function () {
+                        a.innerHTML="Zaky. Riper Tingkat Hode";
+                        a.style.opacity=1;
+                        a.style.animation = 'slide_right 15s';
+                        a.style.animationFillMode='forwards';
+
+                        b.innerHTML="Ucup. Pendiri Gblg Crew Yang Sok Gans";
+                        b.style.opacity=1;
+                        b.style.animation = 'slide_left 15s';
+                        b.style.animationFillMode='forwards';
+
+                        setTimeout(function () {
+                            a.innerHTML="Lala. Adalah Kang Carder Yang Cansya Tapi Aku BElum Meyukainya Mungkin Nati Kalo Dah Ad 80jt";
+                            a.style.opacity=1;
+                            a.style.animation = 'slide_left 15s';
+                            a.style.animationFillMode='forwards';
+
+                            b.innerHTML="<br>";
+                            b.style.opacity=1;
+                            b.style.animation = 'slide_right 15s';
+                            b.style.animationFillMode='forwards';
+
+                            setTimeout(function () {
+
+                                a.innerHTML="Rapely. Seorang anak Gans Tapi Boong";
+                                a.style.opacity=1;
+                                a.style.animation = 'slide_right 15s';
+                                a.style.animationFillMode='forwards';
+
+                                b.innerHTML="Harith. Anak Sadboys yang Buryk";
+                                b.style.opacity=1;
+                                b.style.animation = 'slide_left 15s';
+                                b.style.animationFillMode='forwards';
+
+                                setTimeout(function () {
+
+                                    a.innerHTML="Thank you, Itulah Daftar Nama Team Gblg Crew.";
+                                    a.style.opacity=1;
+                                    a.style.animation = 'slide_left 20s';
+                                    a.style.animationFillMode='forwards';
+
+                                    b.innerHTML="Folow : @Dedeaqil7";
+                                    b.style.opacity=1;
+                                    b.style.animation = 'slide_right 15s';
+                                    b.style.animationFillMode='forwards';
+
+                                },8000)
+
+                            },8000);
+
+                        },8000);
+
+                    },8000);
+
+                },8000);
+
+            },8000);
+
+        },8000);
+    },200);
+};
